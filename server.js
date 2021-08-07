a802ce00f7b2034c24be6e1ae1e0cff0fd25eed9
BAUNET\jsgomez <jsgomez@baufest.com>
2021-08-06 23:15:36 -0500

    se modifica nuevamente

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 //Install express server
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
+
 const express = require('express');
-const path = require('path');
-const configMensaje = require('./configMensaje');
 const app = express(); 
 
-require('dotenv').config()
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
 app.use(express.static(__dirname + '/dist/ruleta'));
 
 app.get('*', function(req,res) {
